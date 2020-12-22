0c43645f74f1c858467deefc0b5925a5572988d2
Davide Silva <davidesilva@hey.com>
2020-12-22 11:20:48 +0000

    day11 part2: solved

--- day11/seating_system.rb
+++ day11/seating_system.rb
@@ -1,32 +1,14 @@
 # frozen_string_literal: true
-# rubocop:disable all
 
-INPUT = File.read(ARGV.first).split
+INPUT = File.read(ARGV.first).split.map(&:chars)
 
-def count_neighbors(input, row, column)
-  max_row = input.size
-  max_column = input[0].size
-  count = 0
-
-  count += 1 if column + 1 < max_column && input[row][column + 1] == '#'
-  count += 1 if column + 1 < max_column && row + 1 < max_row && input[row + 1][column + 1] == '#'
-  count += 1 if row + 1 < max_row && input[row + 1][column] == '#'
-  count += 1 if row + 1 < max_row && column - 1 >= 0 && input[row + 1][column - 1] == '#'
-  count += 1 if row - 1 >= 0 && input[row - 1][column] == '#'
-  count += 1 if column - 1 >= 0 && row - 1 >= 0 && input[row - 1][column - 1] == '#'
-  count += 1 if column - 1 >= 0 && input[row][column - 1] == '#'
-  count += 1 if row - 1 >= 0 && column + 1 < max_row && input[row - 1][column + 1] == '#'
-
-  count
-end
-
-def in_bounds?(input, row, column, x, y)
+def in_bounds?(input, row, column, x, y, multiplier = 1)
   size = input.size
 
-  row + x >= 0 && row + x < size && column + y >= 0 && column + y < size
+  row + (x * multiplier) >= 0 && row + (x * multiplier) < size && column + (y * multiplier) >= 0 && column + (y * multiplier) < size
 end
 
-def count_seats(input, row, column)
+def count_adjacent_seats(input, row, column)
   directions = [[-1, -1], [-1, 0], [-1, 1], [0, -1], [0, 1], [1, -1], [1, 0], [1, 1]]
 
   directions.select do |x, y|
@@ -34,28 +16,36 @@ def count_seats(input, row, column)
   end.size
 end
 
-def part1(input = INPUT)
+def count_visible_seats(input, row, column)
+  directions = [[-1, -1], [-1, 0], [-1, 1], [0, -1], [0, 1], [1, -1], [1, 0], [1, 1]]
+
+  directions.select do |x, y|
+    multiplier = 1
+    multiplier += 1 while in_bounds?(input, row, column, x, y, multiplier) && input[row + x * multiplier][column + y * multiplier] == '.'
+
+    in_bounds?(input, row, column, x, y, multiplier) && input[row + x * multiplier][column + y * multiplier] == '#'
+  end.size
+end
+
+def solve(input, limit)
   new_input = input.dup.map(&:dup)
   previous = 0
   while true
-    i = 0
-    while i < input.size
-      j = 0
-      while j < input.size
-        occupied = count_seats(input, i, j)
+    input.each_with_index do |rows, i|
+      rows.each_with_index do |_, j|
+        occupied = count_visible_seats(input, i, j)
         new_input[i][j] = '#' if occupied.zero? && input[i][j] == 'L'
-        new_input[i][j] = 'L' if occupied >= 4 && input[i][j] == '#'
-        j += 1
+        new_input[i][j] = 'L' if occupied >= limit && input[i][j] == '#'
       end
-      i += 1
     end
     current_occupied_seats = new_input.join.count('#')
     break if previous == current_occupied_seats
+
     previous = current_occupied_seats
     input = new_input.dup.map(&:dup)
   end
-  puts new_input.join("\n")
-  puts current_occupied_seats
+  current_occupied_seats
 end
 
-part1
+puts "Part 1: #{solve(INPUT, 4)}"
+puts "Part 2: #{solve(INPUT, 5)}"
