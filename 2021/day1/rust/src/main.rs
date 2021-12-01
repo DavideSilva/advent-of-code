1f3733c53e08c414b5ef7e2768830454ccff37e2
Davide Silva <davidesilva@hey.com>
2021-12-01 17:15:04 +0000

    add rust solution to day 1

---
+++ 2021/day1/rust/src/main.rs
@@ -0,0 +1,35 @@
+use std::env;
+use std::fs;
+
+fn main() {
+    let args: Vec<String> = env::args().collect();
+    let filename = &args[1];
+
+    let contents = fs::read_to_string(filename).expect("Something went wrong reading the file");
+
+    let lines: Vec<&str> = contents.split_whitespace().collect();
+
+    let depths: Vec<i32> = lines.iter().map(|s| s.parse().unwrap()).collect();
+
+    println!("Part 1: {:?}", measure_increase(&depths));
+
+    println!("Part 2: {:?}", rolling_window(&depths));
+}
+
+fn measure_increase(depths: &Vec<i32>) -> i32 {
+    let iter = depths.windows(2);
+
+    let mut res = 0;
+    for val in iter {
+        if val[1] > val[0] {
+            res = res + 1;
+        }
+    }
+    return res;
+}
+
+fn rolling_window(depths: &Vec<i32>) -> i32 {
+    let sums_of_depths: Vec<i32> = depths.windows(3).map(|d| d.to_vec().iter().sum()).collect();
+
+    return measure_increase(&sums_of_depths);
+}
