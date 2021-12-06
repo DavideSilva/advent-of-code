d1e8424e2c36df4d58e4c8152460affb604acc91
Davide Silva <davidesilva@hey.com>
2021-12-06 17:18:46 +0000

    day2: rust code review

--- 2021/day2/rust/src/main.rs
+++ 2021/day2/rust/src/main.rs
@@ -9,28 +9,29 @@ fn main() {
 
     let commands: Vec<&str> = contents.split("\n").filter(|item| item.len() > 0).collect();
 
-    calculate_depth(&commands);
+    calculate_depth(&commands).expect("Uh oh, error");
     new_calculate_depth(&commands);
 }
 
-fn calculate_depth(commands: &Vec<&str>) {
-    let mut h_pos: i32 = 0;
-    let mut depth: i32 = 0;
+fn calculate_depth(commands: &Vec<&str>) -> Result<u32, ()> {
+    let mut h_pos: u32 = 0;
+    let mut depth: u32 = 0;
 
     for line in commands {
-        let l: Vec<&str> = line.split_whitespace().collect();
-        let action = l[0];
-        let units: i32 = l[1].parse().unwrap();
+        let (action, units_string): (&str, &str) = line.split_once(" ").expect("Couldn't parse the instruction line");
+        let units = units_string.parse::<u32>().expect("Couldn't parse the units");
 
         match action {
-            "forward" => h_pos += units,
-            "down" => depth += units,
-            "up" => depth -= units,
-            _ => unreachable!()
-        }
+            "forward" => Ok(h_pos += units),
+            "down" => Ok(depth += units),
+            "up" => Ok(depth -= units),
+            _ => Err(()),
+        }.expect("Error parsing the instructions");
     }
 
     println!("Position: {:?}, Depth: {:?}, Answer: {:?}", h_pos, depth, h_pos * depth);
+
+    Ok(h_pos * depth)
 }
 
 fn new_calculate_depth(commands: &Vec<&str>) {
