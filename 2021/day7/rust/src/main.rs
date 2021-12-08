501b93e5d0a9541a69dbc719ea9b17fe3cea5667
Davide Silva <davidesilva@hey.com>
2021-12-08 23:38:03 +0000

    day7: add rust solution

---
+++ 2021/day7/rust/src/main.rs
@@ -0,0 +1,68 @@
+use std::env;
+use std::fs;
+
+fn main() {
+    let args: Vec<String> = env::args().collect();
+    let filename = &args[1];
+    let contents = fs::read_to_string(filename).expect("Something went wrong reading the file");
+
+    let crabs: Vec<i32> = contents
+        .trim()
+        .split(",")
+        .map(|i| i.parse().unwrap())
+        .collect();
+
+    println!("{:?}", part1(&crabs));
+    println!("{:?}", part2(&crabs));
+}
+
+fn part1(crabs: &Vec<i32>) -> i32 {
+    let min: i32 = *crabs.iter().min().unwrap();
+    let max: i32 = *crabs.iter().max().unwrap();
+
+    let fuel = *(min..max)
+        .collect::<Vec<i32>>()
+        .iter()
+        .map(|i| {
+            let c = crabs
+                .iter()
+                .map(|c| (i - c).abs())
+                .collect::<Vec<i32>>()
+                .into_iter()
+                .sum::<i32>();
+            c
+        })
+        .collect::<Vec<i32>>()
+        .iter()
+        .min()
+        .unwrap();
+
+    fuel
+}
+
+fn part2(crabs: &Vec<i32>) -> i32 {
+    let min: i32 = *crabs.iter().min().unwrap();
+    let max: i32 = *crabs.iter().max().unwrap();
+
+    let fuel = *(min..max)
+        .collect::<Vec<i32>>()
+        .iter()
+        .map(|i| {
+            let c = crabs
+                .iter()
+                .map(|c| {
+                    let d = (i - c).abs();
+                    d * (d + 1) / 2
+                })
+                .collect::<Vec<i32>>()
+                .into_iter()
+                .sum::<i32>();
+            c
+        })
+        .collect::<Vec<i32>>()
+        .iter()
+        .min()
+        .unwrap();
+
+    fuel
+}
